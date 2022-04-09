e8144fc94a6ce588f3b16fe781fafcebdf975ebc
Black_Penguin <dkstmdwnswns@gmail.com>
2022-04-09 13:22:28 +0900

    SoundManager

---
+++ Assets/Scripts/Manager/Singleton.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public bool isNotDestroy;
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                T t;
+                t = FindObjectOfType(typeof(T)) as T;
+                if (t == null)
+                {
+                    GameObject obj = new GameObject(typeof(T).Name);
+                    obj.AddComponent<T>();
+                    instance = obj.GetComponent<T>();
+                }
+                else
+                {
+                    instance = t.GetComponent<T>();
+                }
+            }
+            else return instance;
+            return instance;
+        }
+    }
+    protected virtual void Awake()
+    {
+        if (!isNotDestroy)
+        {
+            if(instance != null)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                instance = GetComponent<T>();
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+    }
+}
