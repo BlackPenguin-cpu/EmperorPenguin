194a859f1ee16b0fad7f04a1db61afe51b04cf3f
Black_Penguin <dkstmdwnswns@gmail.com>
2022-04-10 19:03:10 +0900

    세이브 완성성?

--- Assets/Scripts/Manager/ShopManager.cs
+++ Assets/Scripts/Manager/ShopManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 
@@ -11,9 +12,14 @@ public class estate
 public class JusikData
 {
     public int Count;
-    public int Value;
+    public long Value;
+    public string Name;
 }
-
+public class JasanData
+{
+    public bool isBuy;
+    public string Name;
+}
 public class SaveData
 {
     public long Coin;
@@ -25,29 +31,61 @@ public class SaveData
 
     public estate[] dataSturctures = new estate[10];
 
-    //public estate roofTop;
-    //public estate halfUnder;
-    //public estate officetell;
-    //public estate multiLiveHouse;
-    //public estate SingleLiveHouse;
-    //public estate apart50;
-    //public estate Buliding10;
-    //public estate Buliding30;
-    //public estate Buliding50;
-    //public estate Hotel;
+    public JasanData[] jasanDatas = new JasanData[5];
 
     public JusikData[] Jusik = new JusikData[5];
 
-    public JusikData Menity;
-    public JusikData Youtube;
-    public JusikData Taslo;
-    public JusikData Nuntendo;
-    public JusikData bitCoin;
 }
 public class ShopManager : MonoBehaviour
 {
 
     private void Start()
+    {
+        string fileName = "SaveData";
+        string Path = Application.persistentDataPath + "/" + fileName + ".Json";
+        string json = File.ReadAllText(Path);
+
+        SaveData saveData = JsonUtility.FromJson<SaveData>(Path);
+        if (saveData == null) return;
+        GameManager.Instance.Coin = saveData.Coin;
+        GameManager.Instance.ClickCoinUp = saveData.ClickCoin;
+        GameManager.Instance.secCoinup = saveData.SecCoin;
+
+        FindObjectOfType<LeaderPenguin>().Level = saveData.leaderPenguinLevel;
+
+        var penguins = FindObjectsOfType<Jobbutton>();
+        foreach (var penguin in penguins)
+            penguin.Level = saveData.PenguinLevel[penguin.Penguinidx];
+
+        var Structures = FindObjectsOfType<Realestate>();
+        int count = 0;
+        foreach (var Structure in Structures)
+        {
+            Structure.ItemName = saveData.dataSturctures[count].Name;
+            Structure.Buy = saveData.dataSturctures[count].isBuy;
+            count++;
+        }
+
+        count = 0;
+        var Jasans = FindObjectsOfType<JasanText>();
+        foreach (JasanText jasan in Jasans)
+        {
+            jasan.ItemName = saveData.jasanDatas[count].Name;
+            jasan.Buy = saveData.jasanDatas[count].isBuy;
+            count++;
+        }
+
+        var Jusiks = FindObjectsOfType<Jusik>();
+        count = 0;
+        foreach (var juiisk in Jusiks)
+        {
+            juiisk.ItemName = saveData.Jusik[count].Name;
+            juiisk.nowValue = saveData.Jusik[count].Value;
+            juiisk.Count = saveData.Jusik[count].Count;
+            count++;
+        }
+    }
+    private void OnApplicationQuit()
     {
         SaveData saveData = new SaveData();
 
@@ -66,9 +104,28 @@ public class ShopManager : MonoBehaviour
             count++;
         }
 
-    }
-    private void OnApplicationQuit()
-    {
+        count = 0;
+        var Jasans = FindObjectsOfType<JasanText>();
+        foreach (JasanText jasan in Jasans)
+        {
+            saveData.jasanDatas[count].Name = jasan.ItemName;
+            saveData.jasanDatas[count].isBuy = jasan.Buy;
+            count++;
+        }
 
+        var Jusiks = FindObjectsOfType<Jusik>();
+        count = 0;
+        foreach (var juiisk in Jusiks)
+        {
+            saveData.Jusik[count].Name = juiisk.ItemName;
+            saveData.Jusik[count].Value = juiisk.nowValue;
+            saveData.Jusik[count].Count = juiisk.Count;
+            count++;
+        }
+
+        string json = JsonUtility.ToJson(saveData);
+        string fileName = "SaveData";
+        string Path = Application.persistentDataPath + "/" + fileName + ".Json";
+        File.WriteAllText(Path, json);
     }
 }
