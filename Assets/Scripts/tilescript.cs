f1fa4a209eede3979041976471b1ac4f47dac5db
Black_Penguin <dkstmdwnswns@gmail.com>
2022-04-10 20:51:24 +0900

    title수정

--- Assets/Scripts/tilescript.cs
+++ Assets/Scripts/tilescript.cs
@@ -6,7 +6,9 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 public class tilescript : MonoBehaviour
 {
-    [SerializeField] GameObject title,titletext;
+    [SerializeField] AudioSource titleAudio;
+
+    [SerializeField] GameObject title, titletext;
     [SerializeField] GameObject CreatorUI;
     [SerializeField] GameObject CreatorButtons;
     [SerializeField] GameObject StartShowObject;
@@ -14,39 +16,44 @@ public class tilescript : MonoBehaviour
     private Color color;
     private void Start()
     {
+        titleAudio.clip = Resources.Load<AudioClip>("Sounds/Title");
+        titleAudio.Play();
         StartCoroutine("titlemove");
         color = titletext.GetComponent<Text>().color;
     }
     IEnumerator titlemove()
     {
-        title.transform.DOLocalMove(Vector3.up*30, 4);
+        title.transform.DOLocalMove(Vector3.up * 30, 4);
         float timer = 1;
-        while(timer > 0)
+        while (timer > 0)
         {
             color.a = timer;
-            timer -= Time.deltaTime/2;
+            timer -= Time.deltaTime / 2;
             yield return null;
         }
-        title.transform.DOLocalMove(Vector3.down*30, 4);
+        title.transform.DOLocalMove(Vector3.down * 30, 4);
         while (timer < 1)
         {
             color.a = timer;
-            timer += Time.deltaTime/2;
+            timer += Time.deltaTime / 2;
             yield return null;
         }
         yield return StartCoroutine("titlemove");
     }
     public void CreatorButton()
     {
+        titleAudio.PlayOneShot(Resources.Load<AudioClip>("Sounds/Button_Click"), 1);
         CreatorUI.transform.DOLocalMove(Vector3.zero, 1).SetEase(Ease.OutSine);
     }
 
     public void CreatorExitButton()
     {
-        CreatorUI.transform.DOLocalMove(Vector3.up*1000, 1).SetEase(Ease.OutSine);
+        titleAudio.PlayOneShot(Resources.Load<AudioClip>("Sounds/Button_Click"), 1);
+        CreatorUI.transform.DOLocalMove(Vector3.up * 1000, 1).SetEase(Ease.OutSine);
     }
     public void StartButton()
     {
+        titleAudio.PlayOneShot(Resources.Load<AudioClip>("Sounds/Button_Click"), 1);
         StartCoroutine("StartShow");
     }
     IEnumerator StartShow()
@@ -62,16 +69,17 @@ public class tilescript : MonoBehaviour
     }
     public void ExitButtons()
     {
+        titleAudio.PlayOneShot(Resources.Load<AudioClip>("Sounds/Button_Click"), 1);
         StartCoroutine("ExitShow");
     }
     IEnumerator ExitShow()
     {
-        CreatorUI.transform.DOLocalMove(Vector3.up * 1000, 1).SetEase(Ease.OutSine);
-        StartShowObject.transform.DOLocalMove(Vector3.up * 2900, 3).SetEase(Ease.InBack);
-        CreatorButtons.transform.DOLocalMove(new Vector3(680, 620, 0), 1);
-        ExitButton.transform.DOLocalMove(new Vector3(850, 620, 0), 1);
-        titletext.SetActive(false);
-        yield return new WaitForSeconds(3f);
+        //CreatorUI.transform.DOLocalMove(Vector3.up * 1000, 1).SetEase(Ease.OutSine);
+        //StartShowObject.transform.DOLocalMove(Vector3.up * 2900, 3).SetEase(Ease.InBack);
+        //CreatorButtons.transform.DOLocalMove(new Vector3(680, 620, 0), 1);
+        //ExitButton.transform.DOLocalMove(new Vector3(850, 620, 0), 1);
+        //titletext.SetActive(false);
+        //yield return new WaitForSeconds(3f);
         Application.Quit();
         yield return null;
     }
